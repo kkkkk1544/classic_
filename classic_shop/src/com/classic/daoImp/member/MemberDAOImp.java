92df8fb242e72c653e2929dc4a8bd6603700473d
kjy <juyeon1203@gmail.com>
2018-01-02 23:27:55 +0900

    18.01.02 KJY memberDAO

--- classic_shop/src/com/classic/daoImp/member/MemberDAOImp.java
+++ classic_shop/src/com/classic/daoImp/member/MemberDAOImp.java
@@ -1,5 +1,9 @@
 package com.classic.daoImp.member;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 import com.classic.dao.member.MemberDAO;
 import com.classic.dto.MemberDTO;
 
@@ -7,9 +11,19 @@ public class MemberDAOImp implements MemberDAO{
 
 //주연 시작
 	@Override
-	public MemberDTO memberSelect(String id, String pw) throws Exception {
+	public MemberDTO memberSelect(String id, String pw) throws Exception{
 		MemberDTO memDTO = null;
 		String sql = "SELECT id, pw FROM member WHERE id=? AND pw=?";
+		Connection conn = null;
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, id);
+		pstmt.setString(2, pw);
+		ResultSet rs = pstmt.executeQuery();
+		if(rs.next()) {
+			memDTO = new MemberDTO();
+			memDTO.setId(rs.getString("id"));
+			memDTO.setPw(rs.getString("pw"));
+		}
 		return memDTO;
 	}
 
@@ -17,6 +31,14 @@ public class MemberDAOImp implements MemberDAO{
 	public MemberDTO memberSelect(String id) throws Exception {
 		MemberDTO memDTO = null;
 		String sql = "SELECT id FROM member WHERE id=?";
+		Connection conn = null;
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, id);
+		ResultSet rs = pstmt.executeQuery();
+		if(rs.next()) {
+			memDTO = new MemberDTO();
+			memDTO.setId(rs.getString("id"));
+		}
 		return memDTO;
 	}
 
@@ -25,6 +47,13 @@ public class MemberDAOImp implements MemberDAO{
 		int insert = 0;
 		String sql = "INSERT INTO member (num, id, pw, phone, mail, grade, indate) "
 				+ "VALUES(member_seq.nextval, ?, ?, ?, ?, 1, sysdate)";
+		Connection conn = null;
+		PreparedStatement pstmt = conn.prepareStatement(sql);
+		pstmt.setString(1, memDTO.getId());
+		pstmt.setString(2, memDTO.getPw());
+		pstmt.setString(3, memDTO.getPhone());
+		pstmt.setString(4, memDTO.getMail());
+		insert = pstmt.executeUpdate();
 		return insert;
 	}
 //주연 끝	
--- classic_shop/src/com/classic/dto/MemberDTO.java
+++ classic_shop/src/com/classic/dto/MemberDTO.java
@@ -23,5 +23,51 @@ public class MemberDTO {
 	private String mail;
 	private int grade;
 	private Date indate;
-
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getMail() {
+		return mail;
+	}
+	public void setMail(String mail) {
+		this.mail = mail;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public Date getIndate() {
+		return indate;
+	}
+	public void setIndate(Date indate) {
+		this.indate = indate;
+	}
+	@Override
+	public String toString() {
+		return "MemberDTO [num=" + num + ", id=" + id + ", pw=" + pw + ", phone=" + phone + ", mail=" + mail
+				+ ", grade=" + grade + ", indate=" + indate + "]";
+	}
 }
