862b05f6d6873238745262ce40306159364a75f4
함혜진 <hammi0213@gmail.com>
2018-01-18 20:55:48 +0900

    ham 1/18

--- classic_shop/WebContent/template/header.jsp
+++ classic_shop/WebContent/template/header.jsp
@@ -113,7 +113,7 @@
 								</li>
 								<li>
 									<a href="<c:url value='/user/cart.do'/>">CART
-										<span class="badge badge-pill badge-secondary" style="background-color: #ccc;">0</span>
+										<span class="badge badge-pill badge-secondary" style="background-color: #ccc;" id="cartSymbol"></span>
 									</a>
 								</li>
 								<li><a href="<c:url value='/user/order.do?num=${loginMem.num}' />">ORDER</a></li>
@@ -137,3 +137,22 @@
 				</div>
 			</div>
 		</nav>
+<script>
+$(function(){
+ 	var cookies = document.cookie.split(";");
+ 	var cartSymbol = document.getElementById("cartSymbol");
+ 	//console.log(cookies);
+ 	var productCount=0;
+ 	var i;
+ 	for(i = 0; i<cookies.length; i++){
+ 		var key = (cookies[i].split("=")[0].trim()).split("_")[2];
+ 		if(key=="name"){
+ 			productCount++;
+ 		}
+ 	}
+ 	cartSymbol.innerHTML = productCount;
+ 	if(productCount!=0){
+ 		cartSymbol.style.background = "red";
+ 	}
+});
+</script>
--- classic_shop/src/com/classic/order/controller/CartListController.java
+++ classic_shop/src/com/classic/order/controller/CartListController.java
@@ -22,37 +22,37 @@ public class CartListController extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		resp.setCharacterEncoding("utf-8");
-		int memNum = Integer.parseInt(req.getParameter("num"));
-		int productNum = Integer.parseInt(req.getParameter("productNum"));
+		int memNum =(req.getParameter("num")!=null)?Integer.parseInt(req.getParameter("num")):0;
+		int productNum =(req.getParameter("productNum")!=null)? Integer.parseInt(req.getParameter("productNum")):0;
 		Connection conn = null;
 		Cookie pNameCookie = null;
 		Cookie priceCookie = null;
 		Cookie quentityCookie = null;
-		
-		try {
-			conn = ClassicDBConnection.getConnection();
-			CartDao cart = new CartDaoImp(conn);
-			WishDTO product = new WishDTO();
-			product = cart.fromWishMoveCart(memNum,productNum);
-			priceCookie = new Cookie("classic_"+productNum+"_price",product.getPrice()+"");
-			priceCookie.setMaxAge(60*60*24*7);
-			pNameCookie = new Cookie("classic_"+productNum+"_name",product.getProductName()+"");
-			pNameCookie.setMaxAge(60*60*24*7);
-			quentityCookie = new Cookie("classic_"+productNum+"_count",product.getWishQuantity()+"");
-			quentityCookie.setMaxAge(60*60*24*7);
-		} catch (Exception e) {
-			e.printStackTrace();
-		} finally {
-			ClassicDBConnection.close(conn);
+		if(memNum!=0 ||productNum != 0) {
+			
+			try {
+				conn = ClassicDBConnection.getConnection();
+				CartDao cart = new CartDaoImp(conn);
+				WishDTO product = new WishDTO();
+				product = cart.fromWishMoveCart(memNum,productNum);
+				priceCookie = new Cookie("classic_"+productNum+"_price",product.getPrice()+"");
+				priceCookie.setMaxAge(60*60*24*7);
+				priceCookie.setPath(req.getContextPath());
+				pNameCookie = new Cookie("classic_"+productNum+"_name",product.getProductName()+"");
+				pNameCookie.setMaxAge(60*60*24*7);
+				pNameCookie.setPath(req.getContextPath());
+				quentityCookie = new Cookie("classic_"+productNum+"_count",product.getWishQuantity()+"");
+				quentityCookie.setMaxAge(60*60*24*7);
+				quentityCookie.setPath(req.getContextPath());
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				ClassicDBConnection.close(conn);
+			}
+			resp.addCookie(priceCookie);
+			resp.addCookie(pNameCookie);
+			resp.addCookie(quentityCookie);
 		}
-		resp.addCookie(priceCookie);
-		resp.addCookie(pNameCookie);
-		resp.addCookie(quentityCookie);
-<<<<<<< HEAD
-		req.getRequestDispatcher("order/cart/cart.jsp").forward(req, resp);
-		//resp.sendRedirect("order/cart/cart.jsp");
-=======
-		resp.sendRedirect("order/cart/list.jsp");
->>>>>>> origin
+		req.getRequestDispatcher("/view/order/cart/list.jsp").forward(req, resp);
 	}
 }
